b85e47d405d91ab88f77a900efed20aa59574518
oden <odentorpet@gmail.com>
2022-07-30 20:31:29 -0700

    * Added support for Token base URI * Fixed supportsInterface method

--- contracts/Vingo.sol
+++ contracts/Vingo.sol
@@ -2,24 +2,17 @@
 pragma solidity ^0.8.9;
 
 import "@divergencetech/ethier/contracts/erc721/ERC721ACommon.sol";
-// import "@divergencetech/ethier/contracts/erc721/BaseTokenURI.sol";
+import "@divergencetech/ethier/contracts/erc721/BaseTokenURI.sol";
 import "@divergencetech/ethier/contracts/sales/FixedPriceSeller.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-/// @title Collective NFT
+/// @title Viking Art Collective NFT
 /// @author @odentorp
-contract Vingo is ERC721ACommon, ERC2981, FixedPriceSeller {
+contract Vingo is ERC721ACommon, BaseTokenURI, ERC2981, FixedPriceSeller {
     using Strings for uint256;
-
-    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
-
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
     constructor(
         string memory name,
@@ -28,42 +21,25 @@ contract Vingo is ERC721ACommon, ERC2981, FixedPriceSeller {
         address payable royaltyReceiver
     )
     ERC721ACommon(name, symbol)
+    BaseTokenURI("")
     FixedPriceSeller(
-        0.001 ether,
+        0.0001 ether,
         // How to white list mints?
+        // TODO(odentorp): freeQuota is not working as expected. I was able to mint 5 NFTs even though freeQuota is 2. Similarly, totalInventory is 3, but I was able to mint 5.
+        // These issues to be fixed in a separate PR.
         Seller.SellerConfig({
-            totalInventory: 3,
-            lockTotalInventory: true,
+            totalInventory: 100,
             maxPerAddress: 0,
             maxPerTx: 0,
-            freeQuota: 2,
-            lockFreeQuota: true,
-            reserveFreeQuota: true
+            freeQuota: 10,
+            lockFreeQuota: false, // can update quota if needed
+            reserveFreeQuota: true,
+            lockTotalInventory: true
         }),
         beneficiary
     )
     {
         _setDefaultRoyalty(royaltyReceiver, 750);
-    }
-
-    function makeAnEpicNFT() public {
-     // Get the current tokenId, this starts at 0.
-    uint256 newItemId = _tokenIds.current();
-console.log("minting,, ", newItemId);
-     // Actually mint the NFT to the sender using msg.sender.
-    _safeMint(msg.sender, newItemId);
-
-    // Set the NFTs data.
-    //_setTokenURI(newItemId, "blah");
-    // _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0TkNpQWdJQ0E4YzNSNWJHVStMbUpoYzJVZ2V5Qm1hV3hzT2lCM2FHbDBaVHNnWm05dWRDMW1ZVzFwYkhrNklITmxjbWxtT3lCbWIyNTBMWE5wZW1VNklERTBjSGc3SUgwOEwzTjBlV3hsUGcwS0lDQWdJRHh5WldOMElIZHBaSFJvUFNJeE1EQWxJaUJvWldsbmFIUTlJakV3TUNVaUlHWnBiR3c5SW1Kc1lXTnJJaUF2UGcwS0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUlhCcFkweHZjbVJJWVcxaWRYSm5aWEk4TDNSbGVIUStEUW84TDNOMlp6ND0iCn0=");
-
-    // Increment the counter for when the next NFT is minted.
-    _tokenIds.increment();
-  }
-
-    /// @notice Entry point for purchase of a single token.
-    function buy() external payable {
-        Seller._purchase(msg.sender, 1);
     }
 
     /**
@@ -74,40 +50,73 @@ console.log("minting,, ", newItemId);
         uint256 num,
         bool
     ) internal override {
-        for (uint256 i = 0; i < num; i++) {
-            _safeMint(to, totalSold() + i);
-        }
+        _safeMint(to, num);
+    }
+
+    /**
+    @notice Mint as an address on one of the early-access lists.
+     */
+     // TODO: handle allow list minting
+    // function mint(
+    //     address to,
+    //     uint256 price,
+    //     bytes32 nonce,
+    //     bytes calldata sig
+    // ) external payable {
+    //     signers.requireValidSignature(
+    //         signaturePayload(to, price, nonce),
+    //         sig,
+    //         usedMessages
+    //     );
+    //     _purchase(to, 1, price);
+    // }
+
+    /**
+    @notice Flag indicating that public minting is open.
+     */
+    bool public publicMinting;
+
+    /**
+    @notice Set the `publicMinting` flag.
+     */
+    function setPublicMinting(bool _publicMinting) external onlyOwner {
+        publicMinting = _publicMinting;
+    }
+
+    /**
+    @dev Public minting method only available when public minting is enabled
+    */
+    function mintPublic(
+        address to
+    ) external payable {
+        require(publicMinting, "Public minting closed");
+        console.log(_baseURI());
+        _purchase(to, 1);
     }
 
     /**
     @dev Required override to select the correct baseTokenURI.
      */
-    // function _baseURI()
-    //     internal
-    //     view
-    //     override(BaseTokenURI, ERC721A)
-    //     returns (string memory)
-    // {
-    //     return BaseTokenURI._baseURI();
-    // }
-
-    /// @notice Prefix for tokenURI return values.
-    string public baseTokenURI = "https://www.vikingart.com/images/vikingart-card.svg?token_id=";
-
-    /// @notice Set the baseTokenURI.
-    function setBaseTokenURI(string memory baseTokenURI_) external onlyOwner {
-        baseTokenURI = baseTokenURI_;
+    function _baseURI()
+        internal
+        view
+        override(BaseTokenURI, ERC721A)
+        returns (string memory)
+    {
+        return BaseTokenURI._baseURI();
     }
 
-    // /// @notice Returns the token's metadata URI.
+    /**
+    @notice If renderingContract is set then returns its tokenURI(tokenId)
+    return value, otherwise returns the standard baseTokenURI + tokenId.
+     */
     function tokenURI(uint256 tokenId)
         public
         view
         override
-        tokenExists(tokenId)
         returns (string memory)
     {
-        return string(abi.encodePacked(baseTokenURI, tokenId.toString()));
+        return super.tokenURI(tokenId);
     }
 
     /**
@@ -120,15 +129,16 @@ console.log("minting,, ", newItemId);
         _setDefaultRoyalty(receiver, feeBasisPoints);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721ACommon, ERC2981)
-        returns (bool)
-    {
-        if(interfaceId == _INTERFACE_ID_ERC2981) {
-          return true;
-        }
-        return super.supportsInterface(interfaceId);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721ACommon, ERC2981) returns (bool) {
+        // Supports the following `interfaceId`s:
+        // - IERC165: 0x01ffc9a7
+        // - IERC721: 0x80ac58cd
+        // - IERC721Metadata: 0x5b5e139f
+        // - IERC2981: 0x2a55205a
+        return 
+            ERC721A.supportsInterface(interfaceId) || 
+            ERC2981.supportsInterface(interfaceId);
     }
 }
