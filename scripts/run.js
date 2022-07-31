102f47f43f9499f2046b0d979cfde60febe459b3
oden <odentorpet@gmail.com>
2022-07-30 21:57:27 -0700

    remove unused run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,11 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-    const nftContract = await nftContractFactory.deploy();
+    const nftContractFactory = await hre.ethers.getContractFactory('Vingo');
+    const nftContract = await nftContractFactory.deploy(
+        "Tester",
+        "TESTSTS",
+        "0xD149b3b88b4Ca9cb9a174a95f2EE492a80AC0EC3",
+        "0x1260443F80a91eA400B055D8825D6a99ee8b81A2"
+    );
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
   };
